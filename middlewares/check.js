846bfdf2615299b35221d893182e16a9f5908c04
jiayeZhu <1352953@tongji.edu.cn>
2016-12-21 13:48:34 +0800

    暂时停用check中间件

--- middlewares/check.js
+++ middlewares/check.js
@@ -2,32 +2,32 @@ var ResData = require("../models/res")
 
 module.exports={
     checkUserLogin: function checkUserLogin(req, res, next) {
-        if (!req.session.user) {
-            resData = new ResData();
-            resData.setIsSuccess(0);
-            resData.setData("user not login");
-            res.send(JSON.stringify(resData));
-        }
+        // if (!req.session.user) {
+        //     resData = new ResData();
+        //     resData.setIsSuccess(0);
+        //     resData.setData("user not login");
+        //     res.send(JSON.stringify(resData));
+        // }
         next();
     },
 
     checkCompanyLogin: function checkCompanyLogin(req, res, next) {
-        if (!req.session.company) {
-            resData = new ResData();
-            resData.setIsSuccess(0);
-            resData.setData("companyUser not login");
-            res.send(JSON.stringify(resData));
-        }
+        // if (!req.session.company) {
+        //     resData = new ResData();
+        //     resData.setIsSuccess(0);
+        //     resData.setData("companyUser not login");
+        //     res.send(JSON.stringify(resData));
+        // }
         next();
     },
 
     checkAdminLogin: function checkAdminLogin(req, res, next) {
-        if (!req.session.admin) {
-            resData = new ResData();
-            resData.setIsSuccess(0);
-            resData.setData("admin not login");
-            res.send(JSON.stringify(resData));
-        }
+        // if (!req.session.admin) {
+        //     resData = new ResData();
+        //     resData.setIsSuccess(0);
+        //     resData.setData("admin not login");
+        //     res.send(JSON.stringify(resData));
+        // }
         next();
     }
 };
